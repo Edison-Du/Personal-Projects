a09befecf28571b6205728a7d1a87a7f7ae1e9fe
Edison-Du <83848572+Edison-Du@users.noreply.github.com>
2021-07-18 01:52:26 -0400

    Add files via upload

--- Analog Clock/script.js
+++ Analog Clock/script.js
@@ -1,37 +1,48 @@
-// Adding numbers to clock
-var numClass = document.getElementsByClassName("numbers");
-for (var i = 1; i <= 12; i++) {
-    numClass.item(0).innerHTML += "<div class=\"num\" id=\"num-"+i+"\"><div id=\"rev-"+i+"\">"+i+"</div></div>";
-}
-for (var i = 1; i <= 12; i++) {
-    var currentNum = document.getElementById("num-"+i);
-    var currentNumReversed = document.getElementById("rev-"+i);
-    currentNum.style.transform = "rotate(" + i*30 + "deg)";
-    currentNumReversed.style.transform = "rotate(" + -i*30 + "deg)";
+function init() {
+    // Adding numbers to clock
+    for (let i = 1; i <= 12; i++) {
+        let number, reverseRotation;
+
+        number = document.createElement("div");
+        number.setAttribute("class","num");
+        number.style.transform = `rotate(${i*30}deg)`;
+
+        reverseRotation = document.createElement("div");
+        reverseRotation.style.transform = `rotate(${-i*30}deg)`;
+        reverseRotation.innerHTML = i;
+
+        number.appendChild(reverseRotation);
+        document.getElementById("numbers").appendChild(number);
+    }
+    // Adding markers to the clock
+    for (let i = 1; i <= 60; i++) {
+        let marker, markerLine;
+
+        marker = document.createElement("div");
+        marker.setAttribute("class", "marker");
+        marker.style.transform = `rotate(${i*6}deg)`;
+
+        markerLine = document.createElement("div");
+        markerLine.setAttribute("class", "marker-line");
+
+        marker.appendChild(markerLine);
+        document.getElementById("numbers").appendChild(marker);
+    }
 }
 
-// Adding markers to the clock
-for (var i = 1; i <= 60; i++) {
-    numClass.item(0).innerHTML += "<div class=\"marker\" id=\"marker-"+i+"\"><div class=\"marker-line\"></div></div>";
-}
-for (var i = 1; i <= 60; i++) {
-    var currentMarker = document.getElementById("marker-"+i);
-    currentMarker.style.transform = "rotate(" + i*6 + "deg)";
-}
+init();
 
 // Moving the hands
 setInterval(moveHands, 1000);
 function moveHands() {
-    var time = new Date();
-    var hr = document.getElementById("hr");
-    var min = document.getElementById("min");
-    var sec = document.getElementById("sec");
-    var totalSecondsDay = time.getHours()*3600 + time.getMinutes()*60 + time.getSeconds();
-    var totalSecondsHour = time.getMinutes()*60 + time.getSeconds();
-    // Make sure it fits in 12 hours, not 24
-    if (totalSecondsDay >= 43200) totalSecondsDay -= 43200;
-    hr.style.transform = "translate(-50%) rotate(" + totalSecondsDay/43200*360 + "deg)";
-    min.style.transform = "translate(-50%) rotate(" + totalSecondsHour/10 + "deg)";
-    sec.style.transform = "translate(-50%) rotate(" + time.getSeconds() * 6 + "deg)";
+    let time = new Date();
+    let hr = document.getElementById("hr");
+    let min = document.getElementById("min");
+    let sec = document.getElementById("sec");
+    let totalSecondsDay = time.getHours()*3600 + time.getMinutes()*60 + time.getSeconds();
+    let totalSecondsHour = time.getMinutes()*60 + time.getSeconds();
+    hr.style.transform = `translate(-50%) rotate(${totalSecondsDay/43200*360}deg)`;
+    min.style.transform = `translate(-50%) rotate(${totalSecondsHour/10}deg)`;
+    sec.style.transform = `translate(-50%) rotate(${time.getSeconds()*6}deg)`;
 }
 moveHands();
